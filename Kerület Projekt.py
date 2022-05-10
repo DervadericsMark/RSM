035382a3fea1204f1dc16f9b93f89618166e5d89
Raheli Bloszom <80382900+MolnarBenceCsaba@users.noreply.github.com>
2022-05-10 11:31:32 +0200

    Alap ablak + Menü Kezdetek

--- Kerület Projekt.py
+++ Kerület Projekt.py
@@ -1,6 +1,39 @@
- RSMUWU-1
-from Tkinter import *
+from tkinter import*
+
+root = Tk()
+
+root.minsize(width=400, height=400)
+
+menubar = Menu(root)
+
+filemenu = Menu(menubar, tearoff=0)
+
+filemenu.add_command(label="Kör")
+
+filemenu.add_command(label="Négyzet")
+
+filemenu.add_command(label="Rombusz")
+
+filemenu.add_command(label="Háromszög")
+
+filemenu.add_command(label="Trapéz")
+
+filemenu.add_command(label="Paralelogramma")
+
+filemenu.add_command(label="Deltoid")
+
+filemenu.add_command(label="Téglalap")
 
 
-from tkinter import *
- main
+filemenu.add_separator()
+
+editmenu = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Alakzatok", menu=filemenu)
+
+
+
+
+root.config(menu=menubar)
+
+root.mainloop()
